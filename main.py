8d4660670ad7bca5b9a6aa941b6ce43a281c5e47
Anonymous <141341450+bpopivnyak@users.noreply.github.com>
2023-08-20 18:26:58 +0300

    дуля з маком

---
+++ main.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+window = QWidget()
+window.resize(500, 500)
+mainLine = QVBoxLayout()
+menuBth = QPushButton("Меню")
+restBth = QPushButton("Відпочити")
+timeSpn = QSpinBox()
+timeLbl = QLabel("хвилин")
+
+firstLine = QHBoxLayout()
+firstLine.addWidget(menuBth)
+firstLine.addWidget(restBth)
+firstLine.addWidget(timeSpn)
+firstLine.addWidget(timeLbl)
+mainLine.addLayout(firstLine)
+
+answersGroup = QGroupBox("Варіанти відповідей")
+answer1 = QRadioButton("1")
+answer2 = QRadioButton("2")
+answer3 = QRadioButton("3")
+answer4 = QRadioButton("4")
+answersLine = QVBoxLayout()
+answersLine.addWidget(answer1)
+answersLine.addWidget(answer2)
+answersLine.addWidget(answer3)
+answersLine.addWidget(answer4)
+answersGroup.setLayout(answersLine)
+
+mainLine.addWidget(answersGroup)
+
+window.setLayout(mainLine)
+window.show()
+app.exec()
